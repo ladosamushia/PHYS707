3dc5f2538742886556dbbf6cde08404a88cf5361
BlaineFry <42702348+BlaineFry@users.noreply.github.com>
2019-10-24 10:17:22 -0500

    Update Phys_707_Model_Selection_v2.py

--- BlaineFry/Phys_707_Model_Selection_v2.py
+++ BlaineFry/Phys_707_Model_Selection_v2.py
@@ -8,12 +8,11 @@ Created on Tue Oct 22 09:52:28 2019
 import numpy as np
 from numpy import random as rand
 from matplotlib import pyplot as plt
-from scipy.optimize import curve_fit
 
 #%% generate some data
-Npoints = 100
+Npoints = 50
 mu_0 = 1.0
-sigma_0 = 2.0
+sigma_0 = 1.0
 data = rand.normal(loc=mu_0,scale=sigma_0,size=Npoints)
 # and make a histogram of it
 xbounds = [-5,5]
@@ -40,8 +39,8 @@ def Gauss_B(x):
 
 
 x = np.linspace(xbounds[0],xbounds[1],num=1000)
-plt.plot(x,mod1(x),'c-',label='Gauss A')
-plt.plot(x,mod2(x),'b-',label='Gauss B')
+plt.plot(x,Gauss_A(x),'c-',label='Gauss A')
+plt.plot(x,Gauss_B(x),'b-',label='Gauss B')
 plt.legend()
 
 #%% start comparing models
@@ -49,10 +48,13 @@ plt.legend()
 # logs would be better... consider revising
 
 Ntrials = 1000
-def compare_models(mu_ex,sigma_ex,model_1,model_2):
+def compare_models(actual_dist,mu_ex,sigma_ex,model_1,model_2): # actual_dist = 'Gauss' or 'Cauchy'
     log_ratios = []
     for i in range(Ntrials):
-        data = rand.normal(loc=mu_ex,scale=sigma_ex,size=Npoints)
+        if actual_dist is 'Gauss':
+            data = rand.normal(loc=mu_ex,scale=sigma_ex,size=Npoints)
+        else:
+            data = rand.standard_cauchy(size=Npoints)
         # find the probability of the data set given model 1
         prob1 = 1
         for i in range(Npoints):
@@ -64,8 +66,8 @@ def compare_models(mu_ex,sigma_ex,model_1,model_2):
         log_ratios.append(np.log10(prob1/prob2))
     return log_ratios
 
-ratios_A = compare_models(1.0,1.0,Gauss_A,Gauss_B) # compare the models if A is true
-ratios_B = compare_models(1.2,1.0,Gauss_A,Gauss_B) # compare the models if B is true
+ratios_A = compare_models('Gauss',1.0,1.0,Gauss_A,Gauss_B) # compare the models if A is true
+ratios_B = compare_models('Gauss',1.2,1.0,Gauss_A,Gauss_B) # compare the models if B is true
 
 plt.figure(2)
 plt.title('Model Comparison')
@@ -73,6 +75,7 @@ plt.ylabel('Normalized Frequency')
 plt.xlabel(r'$\log_{10} \left(\frac{f_A}{f_B}\right)$')
 plt.hist(ratios_A,bins=Ntrials/10,alpha=0.7,normed=True,label='A is True')
 plt.hist(ratios_B,bins=Ntrials/10,alpha=0.7,normed=True,label='B is True')
+plt.legend()
 
 #%% Now we want to do the same, but with Cauchy vs Gauss
 
@@ -85,9 +88,21 @@ def CAUCHY(x):
     return 1.0/((np.pi*sigma_star)*(1.0+(((x-mu_star)/sigma_star)**2)))
 
 plt.figure(3)
+plt.title('Example Distributions')
 x = np.linspace(-5,5,100)
-plt.plot(x,GAUSS(x),'b')
-plt.plot(x,CAUCHY(x),'r-')
+plt.plot(x,GAUSS(x),'b',label='Gauss')
+plt.plot(x,CAUCHY(x),'r-',label='Cauchy')
+plt.legend()
 
+ratios_Gauss = compare_models('Gauss',0.0,1.0,GAUSS,CAUCHY)
+ratios_Cauchy = compare_models('Cauchy',0.0,1.0,GAUSS,CAUCHY)
+
+plt.figure(4)
+plt.title('Gauss vs Cauchy')
+plt.ylabel('Normalized Frequency')
+plt.xlabel(r'$\log_{10} \left(\frac{f_{Gauss}}{f_{Cauchy}}\right)$')
+plt.hist(ratios_Gauss,bins=Ntrials/10,alpha=0.7,normed=True,label='Gauss is True')
+plt.hist(ratios_Cauchy,bins=Ntrials/10,alpha=0.7,normed=True,label='Cauchy is True')
+plt.legend()
 
 
