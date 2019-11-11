e15793af44abff1acd0bc2b6c5b86107a4e0c197
PLucasCVN65 <42663904+PLucasCVN65@users.noreply.github.com>
2019-11-10 21:24:04 -0600

    Update Compute_Posterior_Plucas_CVN65.py
    Corrected the definition of function defining the model.

--- Philip Lucas/Compute_Posterior_Plucas_CVN65.py
+++ Philip Lucas/Compute_Posterior_Plucas_CVN65.py
@@ -22,7 +22,7 @@ Omegatest = np.linspace(0,1,N) #Tested Omega values in range 0 to 1
 results = np.zeros((N,N)) # for storing Chi Squared results
 
 def Ht(H0,omega,z): #function of the model
-    return H0*np.sqrt(omega*((1+z)**3) + 1 + omega)
+    return H0*np.sqrt(omega*((1+z)**3) + 1 - omega)
 def Chi2(H_0t,Omegat): #Function for Chi squared values
     chi_sq = 0
     for i in range(len(Z)):
@@ -34,30 +34,33 @@ for i in range(N): #Nested loops to determine Chi Squared values.
         results[i][j] = Chi2(Htest[i],Omegatest[j])
         
 minimum_chisq = np.where(results == np.min(results))
+Hprime = Htest[minimum_chisq[0]] #H Value at minimum Chi Squared value
+Omegaprime = Omegatest[minimum_chisq[1]] #Omega Value at minimum Chi Squared 
 
 print ('Minimum Chi Squared Value location:', minimum_chisq )
-
-Hprime = Htest[minimum_chisq[0]] #H Value at minimum Chi Squared value
-Omegaprime = Omegatest[minimum_chisq[1]] #Omega Value at minimum Chi Squared Value
+print ('Minimum Chi Squared Value:' , np.min(results))
+print ('Best estimate for H:', Hprime)
+print ('Best estimate for Omega:', Omegaprime)
 
 for i in range(len(Z)): #computing data to plot the fit based on lowest chi squared values
     mod = Ht(Hprime,Omegaprime,Z[i])
     MOD.append(mod)
 
 plt.figure(1) #plot of experimental data
+plt.title('Model and Data')
 plt.xlabel('z')
 plt.ylabel(r'$H_0$')
 plt.errorbar(Z, H, SIGMA, fmt = 'o')
 plt.plot(Z,MOD)
 plt.figure(2)
+plt.title(r'$X^2$')
 plt.xlabel(r'$H_0$')
 plt.ylabel(r'$\Omega_m$')
-plt.contour(Htest,Omegatest,results,np.linspace(0,50,15),colors=['black'])
-plt.figure(3)
-plt.xlabel(r'$H_0$')
-plt.ylabel(r'$\Omega_m$')
-plt.contour(Htest,Omegatest,results,np.linspace(0,100,N))
-plt.figure(4)
-plt.imshow(results)
-plt.xlim(50,100)
-plt.ylim(0,1)
+plt.contour(Htest,Omegatest,results,np.linspace(0,100,15),colors=['black'])
+plt.plot([Hprime], [Omegaprime], marker='o', markersize=3, color="red")
+#plt.figure(3) #fake color map
+#plt.xlabel(r'$H_0$')
+#plt.ylabel(r'$\Omega_m$')
+#plt.contour(Htest,Omegatest,results,np.linspace(0,1000,100*N))
+#plt.figure(4) #color map doesn't quite work
+#plt.imshow(results) 
