e256bdc3a041c792e131453b124158b350afea6e
PLucasCVN65 <42663904+PLucasCVN65@users.noreply.github.com>
2019-11-11 13:16:55 -0600

    Update Compute_Posterior_Plucas_CVN65.py
    finalized

--- Philip Lucas/Compute_Posterior_Plucas_CVN65.py
+++ Philip Lucas/Compute_Posterior_Plucas_CVN65.py
@@ -10,6 +10,7 @@ Philip Lucas
 
 import numpy as np
 import matplotlib.pyplot as plt
+
 from matplotlib import colors
 
 N = 100 #matrix size
@@ -19,11 +20,13 @@ Z = ordered[0] #data extracted from file
 H = ordered[1] #data extracted from file
 SIGMA = ordered[2] #data extracted from file
 MOD = [] # for plotting the 'best' model against the data
+onesigma =[]
+
 
 Htest = np.linspace(50,100,N) #Tested H values in range 50-100
 Omegatest = np.linspace(0,1,N) #Tested Omega values in range 0 to 1
 results = np.zeros((N,N)) # for storing Chi Squared results
-
+Likely = []
 def Ht(H0,omega,z): #function of the model
     return H0*np.sqrt(omega*((1+z)**3) + 1 - omega)
 def Chi2(H_0t,Omegat): #Function for Chi squared values
@@ -39,6 +42,7 @@ for i in range(N): #Nested loops to determine Chi Squared values.
 minimum_chisq = np.where(results == np.min(results))
 Hprime = Htest[minimum_chisq[0]] #H Value at minimum Chi Squared value
 Omegaprime = Omegatest[minimum_chisq[1]] #Omega Value at minimum Chi Squared 
+
 
 for i in range(len(Z)): #computing data to plot the fit based on lowest chi squared values
     mod = Ht(Hprime,Omegaprime,Z[i])
@@ -60,13 +64,19 @@ plt.figure(2)#contour plot of Chi Squared values
 plt.title(r'$X^2$')
 plt.xlabel(r'$H_0$')
 plt.ylabel(r'$\Omega_m$')
-plt.contour(Htest,Omegatest,results,np.linspace(0,100,15),colors=['black'])
-plt.plot([Hprime], [Omegaprime], marker='o', markersize=3, color="red")
-plt.figure(3)
-plt.title(r'$X^2$')
-plt.xlabel(r'$H_0$')
-plt.ylabel(r'$\Omega_m$')
+rr = 1 - results/np.max(results) # normalizing this color map
 plt.pcolormesh(Htest,Omegatest,results, norm= colors.LogNorm(), cmap='inferno')
 plt.colorbar()
-plt.contour(Htest,Omegatest,results,np.linspace(0,100,15),colors=['black'])
+plt.contour(Htest,Omegatest,rr,[0.66,0.99],colors=['white'])
+plt.contour(Htest,Omegatest,results,np.linspace(6.5,10.5,4),colors=['green'])
+plt.contour(Htest,Omegatest,results,np.linspace(12,360,30),colors=['blue'])
 plt.plot([Hprime], [Omegaprime], marker='o', markersize=3, color="red")
+#
+#plt.figure(3)#color map doesn't quite work
+#plt.title(r'$X^2$')
+#plt.xlabel(r'$H_0$')
+#plt.ylabel(r'$\Omega_m$')
+#plt.pcolormesh(Htest,Omegatest,results, norm= colors.LogNorm(), cmap='inferno')
+#plt.colorbar()
+#plt.contour(Htest,Omegatest,results,np.linspace(0,15,15),colors=['white'])
+#plt.plot([Hprime], [Omegaprime], marker='o', markersize=3, color="red")
